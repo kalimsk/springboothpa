c246e6acb1c3cd8d569ea05479605494587909bd
kalimmgm <85920111+kalimmgm@users.noreply.github.com>
2021-08-01 17:30:55 +0530

    initial

---
+++ src/main/java/com/k8s/autoscaller/springbootautoscaller/SpringbootautoscallerApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.autoscaller.springbootautoscaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootautoscallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootautoscallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8s/autoscaller/springbootautoscaller/SpringbootautoscallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.autoscaller.springbootautoscaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootautoscallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
